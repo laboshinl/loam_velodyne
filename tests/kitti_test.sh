5e939629569c8344b02a90daa2610a1a49799043
Martin Velas <mato.velas@gmail.com>
2017-02-20 13:22:39 +0100

    KIITI test for robodev1

--- tests/kitti_test.sh
+++ tests/kitti_test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-DATA_DIR=/media/files/kitti/dataset_odometry_velodyne/sequences/train
-OUTPUT_DIR=/media/files/velodyne-odometry-results/47-LOAM-results
+DATA_DIR=${1:-/media/files/kitti/dataset_odometry_velodyne/sequences/train}
+OUTPUT_DIR=${2:-/media/files/velodyne-odometry-results/47-LOAM-results}
 
 roscore &
 pid_core=$!
@@ -13,7 +13,7 @@ mkdir -p $OUTPUT_DIR
 for seq in $(ls $DATA_DIR)
 do
 	pushd $DATA_DIR/$seq/velodyne/
-		roslaunch loam_velodyne loam_velodyne_noreg.launch > $OUTPUT_DIR/$seq.txt &
+		roslaunch loam_velodyne loam_velodyne_noreg.launch rviz:=false > $OUTPUT_DIR/$seq.txt &
 		pid_mapping=$!
 		sleep 5s
 		rosrun loam_velodyne scanRegistration $(ls *.bin | sort)
