e46136e162e89b4ab52919ca13bb13c55d01ad8d
Martin Velas <mato.velas@gmail.com>
2017-02-22 10:09:12 +0100

    KITTI evaluation in test

--- tests/kitti_test.sh
+++ tests/kitti_test.sh
@@ -1,15 +1,35 @@
 #! /bin/bash
 
 DATA_DIR=${1:-/media/files/kitti/dataset_odometry_velodyne/sequences/train}
-OUTPUT_DIR=${2:-/media/files/velodyne-odometry-results/47-LOAM-results}
+OUTPUT_DIR=${2:-/media/files/velodyne-odometry-results/55-LOAM-results-ref-5e9396}
+POSES_DIR=${3:-/media/kitti/dataset_odometry_poses/poses/}
+EMPTY_EVAL=${4:-/media/files/velodyne-odometry-results/empty-evaluation}
+EVAL_TOOL=${5:-~/workspace/tmp-ivelas/sw/kitti-odometry-devkit/evaluate_odometry}
+
+
+
+function filter_output {
+	file=$1
+	expected_lines=$2
+
+	tmp_file=$(mktemp)
+	grep '^\([[:digit:]e.+-]\+\s*\)\{12\}$' $file | tee $tmp_file
+	found_lines=$(wc -l < $tmp_file)
+	while [ $found_lines -lt $expected_lines ]
+	do
+		tail -n1 $tmp_file
+		found_lines=$(($found_lines+1))
+	done
+	rm $tmp_file
+}
+
+
 
 roscore &
 pid_core=$!
-
 sleep 5s
 
 mkdir -p $OUTPUT_DIR
-
 for seq in $(ls $DATA_DIR)
 do
 	pushd $DATA_DIR/$seq/velodyne/
@@ -21,5 +41,15 @@ do
 		sleep 5s
 	popd
 done
-
 kill $pid_core
+
+
+cp -r $EMPTY_EVAL $OUTPUT_DIR/evaluation
+for i in $OUTPUT_DIR/*.txt
+do
+	lines=$(wc -l < $POSES_DIR/$(basename $i))
+	filter_output $i $lines > $OUTPUT_DIR/evaluation/results/ivelas/data/$(basename $i)
+done
+pushd $OUTPUT_DIR/evaluation
+	$EVAL_TOOL ivelas
+popd
